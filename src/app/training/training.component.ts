9cf0248cfdb19d2eac6f67b799918501ad762a0c
Kalil Peixoto <kalil.peixoto@jupiter.co.ao>
2019-02-14 16:46:08 +0100

    - Exercise data being mapped and served through components

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {TrainingService} from './training.service';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-training',
   templateUrl: './training.component.html',
   styleUrls: ['./training.component.css']
 })
-export class TrainingComponent implements OnInit {
+export class TrainingComponent implements OnInit, OnDestroy {
+  exerciseChangedSubscription: Subscription;
   onGoingTraining = false;
 
-  constructor() { }
+  constructor(private trainingService: TrainingService) { }
 
   ngOnInit() {
+    this.exerciseChangedSubscription = this.trainingService.exerciseChanged.subscribe(exercice => {
+      if (exercice) {
+        this.onGoingTraining = true;
+      } else {
+        this.onGoingTraining = false;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.exerciseChangedSubscription.unsubscribe();
   }
 }
---
+++ src/app/training/training.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {Router} from '@angular/router';
+import {Exercise} from './exercise.model';
+import {Subject} from 'rxjs/Subject';
+
+@Injectable()
+export class TrainingService {
+  exerciseChanged = new Subject<Exercise>();
+
+  private availableExercises: Exercise[] = [
+    { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
+    { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
+    { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
+    { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
+  ];
+  private runnningExercise: Exercise = null;
+
+  constructor(private router: Router) {
+
+  }
+
+  getAvailableExercises() {
+    return this.availableExercises.slice();
+  }
+
+  startExercise(selectedExercise: string) {
+    this.exerciseChanged.next({...this.getAvailableExercises().find(ex => ex.id === selectedExercise)});
+  }
+}
