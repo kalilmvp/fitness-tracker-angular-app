4243ec565fc55f3e6379208f108911c473b85c94
Kalil Peixoto <kalil.peixoto@jupiter.co.ao>
2019-02-18 16:44:01 +0100

    - Created auth module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import {NgModule} from '@angular/core';
+import {SignupComponent} from './signup/signup.component';
+import {LoginComponent} from './login/login.component';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {MaterialModule} from '../material.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+@NgModule({
+  declarations: [
+    SignupComponent,
+    LoginComponent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MaterialModule,
+    FlexLayoutModule
+  ],
+  exports: []
+})
+export class AuthModule {
+
+}
