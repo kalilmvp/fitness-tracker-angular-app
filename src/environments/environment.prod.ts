71db01d17b872f8dfd129a03ed439142e30ab11b
Kalil Peixoto <kalil.peixoto@jupiter.co.ao>
2019-02-18 18:23:54 +0100

    - Application ready for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBJFVudjaZn-2GqAiFqILSmBD8h8wbu8R0',
+    authDomain: 'fitness-angular-app.firebaseapp.com',
+    databaseURL: 'https://fitness-angular-app.firebaseio.com',
+    projectId: 'fitness-angular-app',
+    storageBucket: 'fitness-angular-app.appspot.com',
+    messagingSenderId: '711773094875'
+  }
 };
